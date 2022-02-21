f1011ce0fa8011e0e001216436073e77158df283
Logan Murphy <nagolyhprum@hotmail.com>
2022-02-20 23:31:10 -0500

    update redis environment variable

--- src/redis.ts
+++ src/redis.ts
@@ -5,7 +5,7 @@ export const withRedis = (callback : (req : NextApiRequest & {
     redis : ReturnType<typeof createClient>
 }, res : NextApiResponse) => Promise<void> | void) => async (req : NextApiRequest, res : NextApiResponse) => {
 	const client = createClient({
-		url : process.env.REDIS
+		url : process.env.REDIS_URL
 	});
 	await client.connect();
 	const reqWithRedis = req as NextApiRequest & {
